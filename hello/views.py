b31f2a2e9e3832f1c2f685e963c1896338ef7df6
kiyo <kiyohiro.0310@gmail.com>
2023-10-03 11:13:17 +1100

    Initiated project

---
+++ hello/views.py
@@ -0,0 +1,14 @@
+import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Hello, Django.")
+
+def hello_there(request):
+    # print(request.build_absolute.uri())
+    return render(
+        request,
+        'hello/hello_there.html',
+    )
