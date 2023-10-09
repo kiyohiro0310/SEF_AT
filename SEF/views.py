092d4d3229f9e3d23b3c4565da7d22660a3430d3
kiyo <kiyohiro.0310@gmail.com>
2023-10-10 01:53:37 +1100

    Crete home list, database function

--- SEF/views.py
+++ SEF/views.py
@@ -2,11 +2,15 @@ import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from SEF.models import Pet
+
 # Create your views here.
 def home(request):
+    pets = Pet.objects.all().order_by("-id").values()
     return render(
         request,
         'home.html',
+        context={'pets': pets}
     )
 
 # Pet list
@@ -30,9 +34,13 @@ def about_us(request):
     )
 
 def pet_detail(request):
+    pet_id = request.GET["pet_id"]
+    pet = Pet.objects.get(id=pet_id)
+    print(pet)
     return render(
         request,
-        'pet-detail.html'
+        'pet-detail.html',
+        context={'pet': pet}
     )
 
 def pet_form(request):
