e7780d94d4f9e0f8e165ddee4d76ffe1e3a5e36d
kiyo <kiyohiro.0310@gmail.com>
2023-10-11 01:45:45 +1100

    Create login and signup function

--- SEF/urls.py
+++ SEF/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from SEF import views
+from django.contrib import admin
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -8,5 +9,9 @@ urlpatterns = [
     path("about-us", views.about_us),
     path("pet-detail", views.pet_detail),
     path("pet-form", views.pet_form),
-    path("pet-list", views.pet_list)
+    path("pet-list", views.pet_list),
+    path("signup", views.signup, name="signup"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.signout),
+    path("admin/", admin.site.urls)
 ]
